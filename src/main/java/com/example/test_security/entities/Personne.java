b5b50ff0d337e5091bc967487107916ac131d9be
Walid <baaa82792@gmail.com>
2026-02-24 14:08:02 +0100

    ajout personne

---
+++ src/main/java/com/example/test_security/entities/Personne.java
@@ -0,0 +1,18 @@
+package com.example.test_security.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Personne {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String mail;
+}
